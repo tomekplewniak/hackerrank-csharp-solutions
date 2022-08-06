0aa947aa71f9a118d1e330b51f831b8c6c931da7
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-06 10:32:28 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Warmup/SolveMeFirst/Program.cs
@@ -0,0 +1,8 @@
+﻿int val1 = Convert.ToInt32(Console.ReadLine());
+int val2 = Convert.ToInt32(Console.ReadLine());
+int sum = SolveMeFirst(val1, val2);
+Console.WriteLine(sum);
+
+static int SolveMeFirst(int a, int b)
+{
+}
