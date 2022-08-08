12e92c67813168b1341f346d9c538bad06994959
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-08 19:01:06 +0200

    Setup a test case.

--- HackerRankCSharpSolutions/Algorithms/Warmup/MiniMaxSum/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Warmup/MiniMaxSum/Program.cs
@@ -1,6 +1,7 @@
-﻿List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+﻿var testData = "1 2 3 4 5";
+List<int> arr = testData.TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 
-Result.miniMaxSum(arr);
+Result.MiniMaxSum(arr);
 
 class Result
 {
@@ -11,7 +12,7 @@ class Result
      * The function accepts INTEGER_ARRAY arr as parameter.
      */
 
-    public static void miniMaxSum(List<int> arr)
+    public static void MiniMaxSum(List<int> arr)
     {
 
     }
