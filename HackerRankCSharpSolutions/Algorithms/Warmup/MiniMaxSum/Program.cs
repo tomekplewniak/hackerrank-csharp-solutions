629bb8602354cda7212aa4e4c7db6aef2781acf6
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-08 19:00:17 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Warmup/MiniMaxSum/Program.cs
@@ -0,0 +1,18 @@
+﻿List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+Result.miniMaxSum(arr);
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+
+    }
+}
