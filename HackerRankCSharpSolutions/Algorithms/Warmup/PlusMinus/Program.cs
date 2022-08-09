23a908ab55d5acf758b8c61108ac41259807d899
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-09 16:33:48 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Warmup/PlusMinus/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = Convert.ToInt32(Console.ReadLine().Trim());
+List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+Result.plusMinus(arr);
+
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+
+    }
+
+}
