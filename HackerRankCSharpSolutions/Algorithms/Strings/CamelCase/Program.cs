48fbb19cbce3001e12a857ba535de0dfa2b967e2
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-11 16:07:49 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/CamelCase/Program.cs
@@ -0,0 +1,33 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(string s)
+    {
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        int result = Result.camelcase(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
