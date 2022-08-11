4ae5b8050ff20eb687b77164c4c6b2ed36eba8cc
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-11 14:47:33 +0200

    Setup a test case.

--- HackerRankCSharpSolutions/Algorithms/Warmup/BirthdayCakeCandles/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Warmup/BirthdayCakeCandles/Program.cs
@@ -1,4 +1,10 @@
-﻿class Result
+﻿var testData = "3 2 1 3";
+List<int> candles = testData.TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+int result = Result.BirthdayCakeCandles(candles);
+Console.WriteLine(result);
+
+class Result
 {
 
     /*
@@ -7,29 +13,9 @@
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER_ARRAY candles as parameter.
      */
-
-    public static int birthdayCakeCandles(List<int> candles)
+    public static int BirthdayCakeCandles(List<int> candles)
     {
 
     }
 
-}
-
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
-
-        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
-
-        int result = Result.birthdayCakeCandles(candles);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
 }
