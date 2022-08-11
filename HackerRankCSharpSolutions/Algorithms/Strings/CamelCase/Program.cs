b9f2d67139cbfb1002ca5b2bf701306d356762ba
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-08-11 16:31:46 +0200

    Problem: CamelCase. (#7)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/CamelCase/Program.cs
@@ -0,0 +1,25 @@
+﻿var s = "saveChangesInTheEditor";
+int result = Result.CamelCase(s);
+
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int CamelCase(string s)
+    {
+        var counter = 1;
+        counter += (from c in s
+                     where char.IsUpper(c)
+                     select c)
+                     .Count();
+
+        return counter;
+    }
+}
