f3407f7a618a12b8cc257f56278fdc7b7dd1c494
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-17 16:58:05 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Warmup/Staircase/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = Convert.ToInt32(Console.ReadLine().Trim());
+Result.Staircase(n);
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void Staircase(int n)
+    {
+
+    }
+
+}
