5a78179faf6b820522a4972802dddd5345c21e80
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-08-17 17:08:13 +0200

    Problem: Staircase. (#9)
    * Initial a problem.
    
    * Solve the problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Warmup/Staircase/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = Convert.ToInt32(Console.ReadLine().Trim());
+Result.Staircase(n);
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void Staircase(int n)
+    {
+        string staircase = "";
+        string mark = "#";
+        for (int i = 0; i <= n - 1; i++)
+        {
+            staircase += mark;
+            Console.WriteLine(staircase.PadLeft(n));
+        }
+    }
+
+}
