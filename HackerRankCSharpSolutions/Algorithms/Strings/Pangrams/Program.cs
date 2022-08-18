18c2537836a3a9f54bb4e7d7ac6f72a9b743cfdc
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-18 14:26:01 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/Pangrams/Program.cs
@@ -0,0 +1,19 @@
+﻿string s = Console.ReadLine();
+string result = Result.pangrams(s);
+
+class Result
+{
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string pangrams(string s)
+    {
+
+    }
+
+}
