75c17cf4526035e291313edcaf9ade4ce066accd
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-08-18 16:24:04 +0200

    Problem: Pangrams. (#10)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/Pangrams/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+string firstTestData = "We promptly judged antique ivory buckles for the next prize";
+string secondTestData = "All of the letters of the alphabet are present in the string.";
+
+string firstResult = Result.Pangrams(firstTestData);
+string secondResult = Result.Pangrams(secondTestData);
+
+Console.WriteLine(firstResult);
+Console.WriteLine(secondResult);
+
+class Result
+{
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string Pangrams(string s)
+    {
+        var letters = s.ToUpper()
+            .Distinct();
+
+        var count = 0;
+
+        foreach(char c in letters)
+        {
+            if(c >= 65 && c <= 90)
+            {
+                count++;
+            }
+        }
+
+        if (count == 26)
+        {
+            return "pangram";
+        }
+
+        return "not pangram";
+    }
+}
