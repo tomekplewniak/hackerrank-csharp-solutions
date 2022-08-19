fc7e69c0c0f3381c14e71fa23a14483361e0fd2d
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-19 09:58:50 +0200

    Solve the problem.

--- HackerRankCSharpSolutions/Algorithms/Warmup/TimeConversion/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Warmup/TimeConversion/Program.cs
@@ -5,17 +5,22 @@ Console.WriteLine(result);
 
 class Result
 {
-
     /*
      * Complete the 'timeConversion' function below.
      *
      * The function is expected to return a STRING.
      * The function accepts STRING s as parameter.
      */
-
     public static string TimeConversion(string s)
     {
-        
-    }
+        DateTime time;
+        var result = DateTime.TryParse(s, out time);
 
+        if (result)
+        {
+            return time.TimeOfDay.ToString();
+        }
+
+        throw new ArgumentException("Provided hours in string isn't correct.");
+    }
 }
