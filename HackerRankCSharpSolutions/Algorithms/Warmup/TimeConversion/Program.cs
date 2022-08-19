87741d9a9018be4120fad9a619ab758e1073ba5d
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-08-19 10:05:57 +0200

    Problem: Time Conversion. (#11)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Warmup/TimeConversion/Program.cs
@@ -0,0 +1,26 @@
+﻿string s = "07:05:45PM";
+
+string result = Result.TimeConversion(s);
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+    public static string TimeConversion(string s)
+    {
+        DateTime time;
+        var result = DateTime.TryParse(s, out time);
+
+        if (result)
+        {
+            return time.TimeOfDay.ToString();
+        }
+
+        throw new ArgumentException("Provided hours in string isn't correct.");
+    }
+}
