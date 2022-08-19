fb66a2eb2e66de842d410c39ee188d7374883a23
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-19 09:22:13 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Warmup/TimeConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿string s = Console.ReadLine();
+string result = Result.timeConversion(s);
+Console.WriteLine(result);
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+
+    }
+
+}
