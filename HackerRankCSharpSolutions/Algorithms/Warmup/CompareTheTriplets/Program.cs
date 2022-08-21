6c9cac45d4d4a665b032d69ff26551bbd27b9a5f
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-21 09:10:34 +0200

    Setup a test case.

--- HackerRankCSharpSolutions/Algorithms/Warmup/CompareTheTriplets/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Warmup/CompareTheTriplets/Program.cs
@@ -1,12 +1,11 @@
-﻿
-List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+﻿string aTestData = "5 6 7";
+string bTestData = "3 6 10";
 
-List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
-
-List<int> result = Result.compareTriplets(a, b);
+List<int> a = aTestData.TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+List<int> b = bTestData.TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+List<int> result = Result.CompareTriplets(a, b);
 
 Console.WriteLine(String.Join(" ", result));
-
 
 class Result
 {
@@ -20,7 +19,7 @@ class Result
      *  2. INTEGER_ARRAY b
      */
 
-    public static List<int> compareTriplets(List<int> a, List<int> b)
+    public static List<int> CompareTriplets(List<int> a, List<int> b)
     {
 
     }
