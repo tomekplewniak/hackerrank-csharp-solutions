7df9b75614d6a29612a5a4d7bc88c4f399b43359
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-21 09:08:40 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Warmup/CompareTheTriplets/Program.cs
@@ -0,0 +1,28 @@
+﻿
+List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+List<int> result = Result.compareTriplets(a, b);
+
+Console.WriteLine(String.Join(" ", result));
+
+
+class Result
+{
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+
+    }
+
+}
