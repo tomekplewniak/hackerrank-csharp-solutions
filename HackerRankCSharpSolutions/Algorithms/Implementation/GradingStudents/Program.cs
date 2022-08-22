5c1775a7b3ba620b88a43a1dbf1757a65e4a87aa
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-22 15:38:32 +0200

    Setup a test case.

--- HackerRankCSharpSolutions/Algorithms/Implementation/GradingStudents/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Implementation/GradingStudents/Program.cs
@@ -1,29 +1,25 @@
-﻿int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
-List<int> grades = new List<int>();
-
-for (int i = 0; i < gradesCount; i++)
+﻿List<int> grades = new List<int>()
 {
-    int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
-    grades.Add(gradesItem);
-}
+    73,
+    67,
+    38,
+    33
+};
 
-List<int> result = Result.gradingStudents(grades);
+List<int> result = Result.GradingStudents(grades);
 
 Console.WriteLine(String.Join("\n", result));
 
 class Result
 {
-
     /*
      * Complete the 'gradingStudents' function below.
      *
      * The function is expected to return an INTEGER_ARRAY.
      * The function accepts INTEGER_ARRAY grades as parameter.
      */
-
-    public static List<int> gradingStudents(List<int> grades)
+    public static List<int> GradingStudents(List<int> grades)
     {
 
     }
-
 }
