dab39ebd68a1cbff543629326e2ffe74e6b3c84c
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-23 10:49:17 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/FunnyString/Program.cs
@@ -0,0 +1,24 @@
+﻿int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+for (int qItr = 0; qItr < q; qItr++)
+{
+    string s = Console.ReadLine();
+    string result = Result.funnyString(s);
+
+    Console.WriteLine(result);
+}
+
+
+class Result
+{
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+    public static string funnyString(string s)
+    {
+
+    }
+}
