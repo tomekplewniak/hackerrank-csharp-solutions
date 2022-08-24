4cb1c7ade27ee211a3a74a7dff68991a9dd692f7
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-24 15:27:35 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Implementation/CatsAndAMouse/Program.cs
@@ -0,0 +1,21 @@
+﻿int q = Convert.ToInt32(Console.ReadLine());
+
+for (int qItr = 0; qItr < q; qItr++)
+{
+    string[] xyz = Console.ReadLine().Split(' ');
+
+    int x = Convert.ToInt32(xyz[0]);
+
+    int y = Convert.ToInt32(xyz[1]);
+
+    int z = Convert.ToInt32(xyz[2]);
+
+    string result = catAndMouse(x, y, z);
+
+    Console.WriteLine(result);
+}
+
+static string catAndMouse(int x, int y, int z)
+{
+
+}
