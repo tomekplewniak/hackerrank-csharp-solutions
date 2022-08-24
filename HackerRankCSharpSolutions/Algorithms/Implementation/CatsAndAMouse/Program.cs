364769781ad071336506ddf5ece26abdc2680df9
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-24 15:41:15 +0200

    Solve the problem.

--- HackerRankCSharpSolutions/Algorithms/Implementation/CatsAndAMouse/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Implementation/CatsAndAMouse/Program.cs
@@ -5,12 +5,30 @@ int x = Convert.ToInt32(xyz[0]);
 int y = Convert.ToInt32(xyz[1]);
 int z = Convert.ToInt32(xyz[2]);
 
-string result = catAndMouse(x, y, z);
+string result = CatAndMouse(x, y, z);
 
 Console.WriteLine(result);
 
 
-static string catAndMouse(int x, int y, int z)
+static string CatAndMouse(int x, int y, int z)
 {
+    var catA = 0;
+    var catB = 0;
 
+    catA = Math.Abs(x - z);
+    catB = Math.Abs(y - z);
+
+    if (catA < catB)
+    {
+        return "Cat A";
+    }
+    else
+    {
+        if (catA == catB)
+        {
+            return "Mouse C";
+        }
+
+        return "Cat B";
+    }
 }
