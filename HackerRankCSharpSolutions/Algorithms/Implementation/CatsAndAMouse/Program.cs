8da8c9289b98847540ce6cdff19d2cf4951ad47a
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-24 15:36:53 +0200

    Setup a test case.s

--- HackerRankCSharpSolutions/Algorithms/Implementation/CatsAndAMouse/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Implementation/CatsAndAMouse/Program.cs
@@ -1,19 +1,14 @@
-﻿int q = Convert.ToInt32(Console.ReadLine());
+﻿
+string[] xyz = "1 2 3".Split(' ');
 
-for (int qItr = 0; qItr < q; qItr++)
-{
-    string[] xyz = Console.ReadLine().Split(' ');
+int x = Convert.ToInt32(xyz[0]);
+int y = Convert.ToInt32(xyz[1]);
+int z = Convert.ToInt32(xyz[2]);
 
-    int x = Convert.ToInt32(xyz[0]);
+string result = catAndMouse(x, y, z);
 
-    int y = Convert.ToInt32(xyz[1]);
+Console.WriteLine(result);
 
-    int z = Convert.ToInt32(xyz[2]);
-
-    string result = catAndMouse(x, y, z);
-
-    Console.WriteLine(result);
-}
 
 static string catAndMouse(int x, int y, int z)
 {
