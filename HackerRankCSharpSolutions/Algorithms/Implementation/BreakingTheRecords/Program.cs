6d3c58837efb4ba1937eb385586af3f13fde4166
Tomek Plewniak <tomekplewniak@gmail.com>
2022-08-26 16:21:04 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Implementation/BreakingTheRecords/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+List<int> scores = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(scoresTemp => Convert.ToInt32(scoresTemp)).ToList();
+
+List<int> result = Result.BreakingRecords(scores);
+
+Console.WriteLine(String.Join(" ", result));
+
+class Result
+{
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<int> BreakingRecords(List<int> scores)
+    {
+
+    }
+}
