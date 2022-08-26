46a2e109a46f82aa6126eaa19359e97d432dcf88
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-08-26 17:38:21 +0200

    Problem: The Hurdle Race. (#19)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Implementation/TheHurdleRace/Program.cs
@@ -0,0 +1,37 @@
+﻿int n = 5;
+int k = 4;
+
+var testData = "1 6 3 5 2";
+
+List<int> height = testData.TrimEnd().Split(' ').ToList()
+    .Select(heightTemp => Convert.ToInt32(heightTemp)).ToList();
+
+int result = Result.HurdleRace(k, height);
+
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'hurdleRace' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY height
+     */
+
+    public static int HurdleRace(int k, List<int> height)
+    {
+        int drinks = 0;
+
+        var max = height.Max();
+
+        if (max > k)
+        {
+            drinks = max - k;
+        }
+
+        return drinks;
+    }
+}
