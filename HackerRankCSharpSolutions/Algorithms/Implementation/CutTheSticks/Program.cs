05caa35984f9f71857f5924ac338b690ab31b5c4
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-01 10:11:14 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Implementation/CutTheSticks/Program.cs
@@ -0,0 +1,19 @@
+﻿List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+List<int> result = Result.CutTheSticks(arr);
+
+Console.WriteLine(String.Join("\n", result));
+
+class Result
+{
+    /*
+     * Complete the 'cutTheSticks' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<int> CutTheSticks(List<int> arr)
+    {
+
+    }
+}
