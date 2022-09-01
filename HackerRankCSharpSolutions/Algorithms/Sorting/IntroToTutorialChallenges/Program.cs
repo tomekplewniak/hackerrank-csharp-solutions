5e1fb10fc7f22e99fbbff00410912a3acd50065a
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-01 14:46:08 +0200

    Setup a test case.

--- HackerRankCSharpSolutions/Algorithms/Sorting/IntroToTutorialChallenges/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Sorting/IntroToTutorialChallenges/Program.cs
@@ -1,13 +1,8 @@
-﻿
-int V = Convert.ToInt32(Console.ReadLine().Trim());
-int n = Convert.ToInt32(Console.ReadLine().Trim());
+﻿int v = 4;
+List<int> arr = "1 4 5 7 9 12".TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 
-List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
-
-int result = Result.introTutorial(V, arr);
-
+int result = Result.IntroTutorial(v, arr);
 Console.WriteLine(result);
-
 
 class Result
 {
@@ -19,7 +14,7 @@ class Result
      *  1. INTEGER V
      *  2. INTEGER_ARRAY arr
      */
-    public static int introTutorial(int V, List<int> arr)
+    public static int IntroTutorial(int v, List<int> arr)
     {
 
     }
