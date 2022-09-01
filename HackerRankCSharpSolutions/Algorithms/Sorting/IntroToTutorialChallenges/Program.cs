2e4f5a214831990f3ec2b38e62d5a21a88331da8
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-01 14:44:58 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Sorting/IntroToTutorialChallenges/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int V = Convert.ToInt32(Console.ReadLine().Trim());
+int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+int result = Result.introTutorial(V, arr);
+
+Console.WriteLine(result);
+
+
+class Result
+{
+    /*
+     * Complete the 'introTutorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER V
+     *  2. INTEGER_ARRAY arr
+     */
+    public static int introTutorial(int V, List<int> arr)
+    {
+
+    }
+}
