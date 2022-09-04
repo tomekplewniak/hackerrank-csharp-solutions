5d3563ff956876a944b6e5fc2595934499aa6105
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-09-04 17:48:35 +0200

    Problem: Viral Advertising. (#23)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Implementation/ViralAdvertising/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = 3;
+int result = Result.ViralAdvertising(n);
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+    public static int ViralAdvertising(int n)
+    {
+        int shared = 5;
+        int liked = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            var likedPerDay = shared / 2;
+            shared = likedPerDay * 3;
+            liked += likedPerDay;
+        }
+
+        return liked;
+    }
+}
