9754c90ae3b73c327b239dfecb29ccc9396abe09
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-04 17:30:27 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Implementation/ViralAdvertising/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+int result = Result.ViralAdvertising(n);
+
+Console.WriteLine(result);
+class Result
+{
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+    public static int ViralAdvertising(int n)
+    {
+
+    }
+}
