8046e5e3b03be8dca45a86e086e1acfbdd30c5f2
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-05 19:01:30 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/TwoStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿string s1 = Console.ReadLine();
+string s2 = Console.ReadLine();
+string result = Result.TwoStrings(s1, s2);
+Console.WriteLine(result);
+
+class Result
+{
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static string TwoStrings(string s1, string s2)
+    {
+
+    }
+
+}
