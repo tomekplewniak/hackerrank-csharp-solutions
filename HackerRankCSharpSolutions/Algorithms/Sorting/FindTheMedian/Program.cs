2a6345cf3149291644c6753b045d3b5aa6520fd4
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-07 16:01:00 +0200

    Setup a test case.

--- HackerRankCSharpSolutions/Algorithms/Sorting/FindTheMedian/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Sorting/FindTheMedian/Program.cs
@@ -1,5 +1,5 @@
-﻿int n = Convert.ToInt32(Console.ReadLine().Trim());
-List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+﻿var testData = "0 1 2 4 6 5 3";
+List<int> arr = testData.TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 int result = Result.FindMedian(arr);
 Console.WriteLine(result);
 
