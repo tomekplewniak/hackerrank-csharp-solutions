1e7baf4f7f3c915773900d06cfe9623c5ba91f1b
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-07 15:58:41 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Sorting/FindTheMedian/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = Convert.ToInt32(Console.ReadLine().Trim());
+List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+int result = Result.FindMedian(arr);
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static int FindMedian(List<int> arr)
+    {
+
+    }
+}
