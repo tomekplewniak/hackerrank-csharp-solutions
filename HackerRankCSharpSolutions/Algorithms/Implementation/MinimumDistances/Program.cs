dcc2070a4db67502931c88e569b877a0ede9a8b5
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-11 19:30:21 +0200

    Setup a test case.

--- HackerRankCSharpSolutions/Algorithms/Implementation/MinimumDistances/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Implementation/MinimumDistances/Program.cs
@@ -1,5 +1,6 @@
-﻿List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
-int result = Result.minimumDistances(a);
+﻿var testData = "7 1 3 4 1 7";
+List<int> a = testData.TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+int result = Result.MinimumDistances(a);
 Console.WriteLine(result);
 
 class Result
@@ -10,7 +11,7 @@ class Result
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER_ARRAY a as parameter.
      */
-    public static int minimumDistances(List<int> a)
+    public static int MinimumDistances(List<int> a)
     {
 
     }
