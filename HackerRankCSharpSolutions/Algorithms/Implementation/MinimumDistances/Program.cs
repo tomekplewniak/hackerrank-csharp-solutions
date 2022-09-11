42629999748010567970c4ee9cf10b1a8feaea5b
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-11 19:27:27 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Implementation/MinimumDistances/Program.cs
@@ -0,0 +1,17 @@
+﻿List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+int result = Result.minimumDistances(a);
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+    public static int minimumDistances(List<int> a)
+    {
+
+    }
+}
