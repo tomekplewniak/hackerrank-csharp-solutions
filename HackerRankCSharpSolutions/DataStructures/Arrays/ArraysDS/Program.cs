eaac3221926a2556e861609d0a6be9f4b0baf0bf
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-09-12 20:12:41 +0200

    Problem: Arrays - DS. (#27)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ HackerRankCSharpSolutions/DataStructures/Arrays/ArraysDS/Program.cs
@@ -0,0 +1,20 @@
+﻿var testData = "1 4 3 2";
+List<int> arr = testData.TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+List<int> res = Result.ReverseArray(arr);
+Console.WriteLine(String.Join(" ", res));
+
+class Result
+{
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<int> ReverseArray(List<int> a)
+    {
+        a.Reverse();
+        return a;
+    }
+}
