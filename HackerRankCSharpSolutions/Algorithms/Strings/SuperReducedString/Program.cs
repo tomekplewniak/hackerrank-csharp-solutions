3f14c5ee4cb57d228ef78121d57d8bda93924445
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-15 18:11:27 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/SuperReducedString/Program.cs
@@ -0,0 +1,17 @@
+﻿string s = Console.ReadLine();
+string result = Result.SuperReducedString(s);
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'superReducedString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+    public static string SuperReducedString(string s)
+    {
+
+    }
+}
