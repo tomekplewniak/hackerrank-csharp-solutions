37abe3e5a6a5b2ea869e7f824c1abf8a00ae8af3
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-09-15 18:47:13 +0200

    Problem: Super Reduced String. (#29)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/SuperReducedString/Program.cs
@@ -0,0 +1,25 @@
+﻿string s = "aaabccddd";
+string result = Result.SuperReducedString(s);
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'superReducedString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+    public static string SuperReducedString(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in s)
+        {
+            var last = stack.Any() ? stack.Peek() : char.MinValue;
+            if (c == last) 
+                stack.Pop();
+            else stack.Push(c);
+        }
+        return stack.Any() ? new string(stack.Reverse().ToArray()) : "Empty String";
+    }
+}
