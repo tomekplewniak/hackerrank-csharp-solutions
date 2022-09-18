ea63c5d1ddbffb135f87677e31c034d2833ccb1f
Tomek Plewniak <37481429+tomekplewniak@users.noreply.github.com>
2022-09-18 12:25:56 +0200

    Problem: Mars Exploration. (#30)
    * Initial a problem.
    
    * Setup a test case.
    
    * Solve the problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/MarsExploration/Program.cs
@@ -0,0 +1,52 @@
+﻿
+string s = "SOSOOSOSOSOSOSSOSOSOSOSOSOS";
+
+int result = Result.MarsExploration(s);
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+    public static int MarsExploration(string s)
+    {
+        char[] sos = new char[] { 'S', 'O', 'S' };
+        
+        var length = s.Length;
+        var index = 3;
+        var globalDiff = 0;
+
+        for (int i = 0; i < length / 3; i++)
+        {
+            int diff = 0;
+            IEnumerable<char> partOfMessage;
+
+            if (index == 3)
+            {
+                partOfMessage = s.Take(index);    
+            }
+            else
+            {
+                partOfMessage = s.Skip(index - 3).Take(index - (index - 3));
+            }
+            
+            index += 3;
+
+            for (int c = 0; c < 3; c++)
+            {
+                if (sos[c] != partOfMessage.ToArray()[c])
+                {
+                    diff ++;
+                }
+            }
+
+            globalDiff += diff;
+        }
+
+        return globalDiff;
+    }
+}
