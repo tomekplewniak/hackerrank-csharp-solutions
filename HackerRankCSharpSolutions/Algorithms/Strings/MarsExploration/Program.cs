92786694a2d5849018253987d94b6b925bd5a4f6
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-18 11:42:51 +0200

    Initial a problem.

---
+++ HackerRankCSharpSolutions/Algorithms/Strings/MarsExploration/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string s = Console.ReadLine();
+
+int result = Result.MarsExploration(s);
+Console.WriteLine(result);
+
+class Result
+{
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+    public static int MarsExploration(string s)
+    {
+
+    }
+}
