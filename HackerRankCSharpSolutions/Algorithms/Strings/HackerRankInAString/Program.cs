6bebe1fb076c0bf91af5ea0a167be8b820890b46
Tomek Plewniak <tomekplewniak@gmail.com>
2022-09-18 13:10:42 +0200

    Solve the problem.

--- HackerRankCSharpSolutions/Algorithms/Strings/HackerRankInAString/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Strings/HackerRankInAString/Program.cs
@@ -1,11 +1,10 @@
-﻿string s = "hereiamstackerrank";
+﻿string s = "rhbaasdndfsdskgbfefdbrsdfhuyatrjtcrtyytktjjt";
 
 string result = Result.HackerrankInString(s);
 Console.WriteLine(result);
 
 class Result
 {
-
     /*
      * Complete the 'hackerrankInString' function below.
      *
@@ -15,6 +14,24 @@ class Result
 
     public static string HackerrankInString(string s)
     {
-        
+        const string searchWord = "hackerrank";
+
+        if (string.IsNullOrEmpty(s) || s.Length < searchWord.Length)
+            return "NO";
+
+        int matched = 0;
+
+        for (int i = 0, j = 0; i < s.Length; i++)
+        {
+            if (s[i] == searchWord[j])
+            {
+                matched++;
+                j++;
+            }
+
+            if (matched == searchWord.Length)
+                return "YES";
+        }
+        return "NO";
     }
 }
