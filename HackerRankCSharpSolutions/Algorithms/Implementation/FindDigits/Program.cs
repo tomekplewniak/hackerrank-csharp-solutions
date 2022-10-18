06302da5c95b0fb1983c66306462d683a33a3e9e
Tomek Plewniak <tomekplewniak@gmail.com>
2022-10-18 18:13:28 +0200

    Setup a test case.

--- HackerRankCSharpSolutions/Algorithms/Implementation/FindDigits/Program.cs
+++ HackerRankCSharpSolutions/Algorithms/Implementation/FindDigits/Program.cs
@@ -1,52 +1,18 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿int n = 1012;
+int result = Result.FindDigits(n);
+
+Console.WriteLine(result);
 
 class Result
 {
-
     /*
      * Complete the 'findDigits' function below.
      *
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER n as parameter.
      */
-
-    public static int findDigits(int n)
+    public static int FindDigits(int n)
     {
-
-    }
-
-}
-
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int t = Convert.ToInt32(Console.ReadLine().Trim());
-
-        for (int tItr = 0; tItr < t; tItr++)
-        {
-            int n = Convert.ToInt32(Console.ReadLine().Trim());
-
-            int result = Result.findDigits(n);
-
-            textWriter.WriteLine(result);
-        }
-
-        textWriter.Flush();
-        textWriter.Close();
+        throw new NotImplementedException();
     }
 }
